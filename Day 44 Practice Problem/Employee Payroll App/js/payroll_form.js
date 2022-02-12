1c3566e388bc797fbbae47a6a4a4f2cbebaca9f6
Vivek Singh <94423696+vvivksingh@users.noreply.github.com>
2022-02-12 20:28:03 +0530

    ability to reset form on clicking reset

--- Day 44 Practice Problem/Employee Payroll App/js/payroll_form.js
+++ Day 44 Practice Problem/Employee Payroll App/js/payroll_form.js
@@ -122,7 +122,7 @@ window.addEventListener("DOMContentLoaded", () => {
 const save = () => {
     try {
         let employeePayrollData = createEmployeePayrollObject();
-        updateLocalStorage(employeePayrollData);
+        if (employeePayrollData != undefined) updateLocalStorage(employeePayrollData);
     } catch (submitError) {
         alert(submitError);
         return;
@@ -173,3 +173,41 @@ function updateLocalStorage(employeePayrollData) {
     alert("Local Storage Updated Successfully!\nTotal Employees : " + employeePayrollList.length);
     localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
+
+const resetForm = () => {
+    setDefaultValue("#name", "");
+    setDefautlText(".name-error");
+    setDefautlText(".valid-name");
+    unsetSelectedValues("[name=profile]");
+    unsetSelectedValues("[name=gender]");
+    unsetSelectedValues("[name=department]");
+    resetRange("#salary", ".salary-output");
+    setDefaultValue("#day", "1");
+    setDefaultValue("#month", "January");
+    setDefaultValue("#year", "2020");
+    setDefautlText(".startDate-error");
+    setDefautlText(".valid-startDate");
+    setDefaultValue("#notes", "");
+};
+
+const setDefaultValue = (propertyId, value) => {
+    const element = document.querySelector(propertyId);
+    element.value = value;
+};
+
+const unsetSelectedValues = (propertyName) => {
+    allValues = document.querySelectorAll(propertyName);
+    allValues.forEach(input => input.checked == false);
+};
+
+const resetRange = (propertyId, outputId) => {
+    const rangeElement = document.querySelector(propertyId);
+    rangeElement.value = 400000;
+    const outputElement = document.querySelector(outputId);
+    outputElement.textContent = rangeElement.value;
+};
+
+const setDefautlText = (propertyId) => {
+    const contentElement = document.querySelector(propertyId);
+    contentElement.textContent = "";
+};
